c4cd219c440ffdd1aebde1e27070f401ad427620
Roman <tretyak_roma@ukr.net>
2015-01-16 10:05:58 +0000

    -
    -

---
+++ FtpSendFile.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace QuelleExportXml
+{
+    class FtpSendFile
+    {
+        public void FTPUploadFile(string filename)
+        {
+            FileInfo fileInf = new FileInfo(filename);
+            string uri = "ftp://" + "ftp.name.ru" + "/folder_root/folder/" + fileInf.Name;
+            FtpWebRequest reqFTP;
+
+            reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri("ftp://" + "ftp.name.ru" + "/folder_root/folder/" + fileInf.Name));
+            reqFTP.Credentials = new NetworkCredential("Login", "pass");
+
+            reqFTP.KeepAlive = false;
+            reqFTP.Method = WebRequestMethods.Ftp.UploadFile;
+            reqFTP.UseBinary = true;
+            reqFTP.ContentLength = fileInf.Length;
+
+            int buffLength = 2048;
+            byte[] buff = new byte[buffLength];
+            int contentLen;
+
+            FileStream fs = fileInf.OpenRead();
+
+            try
+            {
+                Stream strm = reqFTP.GetRequestStream();
+                contentLen = fs.Read(buff, 0, buffLength);
+                while (contentLen != 0)
+                {
+                    strm.Write(buff, 0, contentLen);
+                    contentLen = fs.Read(buff, 0, buffLength);
+                }
+
+                strm.Close();
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+    }
+}
---
+++ SendEmail.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Mail;
+
+namespace BuildEmail
+{
+    class SendEmail
+    {
+
+/// <summary>
+ /// Отправка письма на почтовый ящик C# mail send
+ /// </summary>
+ /// <param name="smtpServer">Имя SMTP-сервера</param>
+ /// <param name="from">Адрес отправителя</param>
+ /// <param name="password">пароль к почтовому ящику отправителя</param>
+ /// <param name="mailto">Адрес получателя</param>
+ /// <param name="caption">Тема письма</param>
+ /// <param name="message">Сообщение</param>
+ /// <param name="attachFile">Присоединенный файл</param>
+        public void SendMail(string smtpServer, string from, string password,
+       string mailto1, string mailto2, string mailto3, string mailto4, string caption, string message, string attachFile = null)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(from);
+                mail.To.Add(new MailAddress(mailto1));
+                mail.To.Add(new MailAddress(mailto2));
+                mail.To.Add(new MailAddress(mailto3));
+                mail.To.Add(new MailAddress(mailto4));
+                mail.Subject = caption;
+                mail.Body = message;
+                if (!string.IsNullOrEmpty(attachFile))
+                    mail.Attachments.Add(new Attachment(attachFile));
+                SmtpClient client = new SmtpClient();
+                client.Host = smtpServer;
+                client.Port = 587;
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential(from.Split('@')[0], password);
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.Send(mail);
+                mail.Dispose();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Mail.Send: " + e.Message);
+            }
+        }
+
+
+    }
+}
