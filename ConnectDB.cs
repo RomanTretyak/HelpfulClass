0812d80a20510b62de80cf62c6b531fb79b8aa89
Roman <tretyak_roma@ukr.net>
2015-01-16 09:57:53 +0000

    -
    -

---
+++ ConnectDB.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace ITClaims
+{
+    class ConnectDB
+    {
+        //public SqlConnection myConn = new SqlConnection("Server=166.168.169.7;Database=Base;Integrated Security=true;");
+        public SqlConnection myConn = new SqlConnection("Server=166.168.169.7;Database=Base;User ID=Name;Password=pass;");
+
+        public DataTable SelectData(string str)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                if (myConn.State == ConnectionState.Open)
+                    myConn.Close();
+                
+              //  ConfigurationManager.AppSettings["ConnectDB"].ToString();
+                
+                myConn.Open();
+                SqlDataAdapter sd = new SqlDataAdapter(str, myConn);
+                sd.Fill(dt);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.ToString());
+                
+            }
+            finally
+            {
+                if (myConn.State == ConnectionState.Open)
+                    myConn.Close();
+
+            }
+            return dt;
+        }
+
+        public bool InsertDB(string str)
+        {
+            try
+            {
+                SqlCommand myCommand = new SqlCommand(str, myConn);
+
+                if (myConn.State == ConnectionState.Open)
+                    myConn.Close();
+
+                myConn.Open();
+                myCommand.ExecuteScalar();
+                return true;
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.ToString());
+                return false;
+            }
+            finally
+            {
+                if (myConn.State == ConnectionState.Open)
+                    myConn.Close();
+            }
+        }
+    }
+}
---
+++ cLogger.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Configuration;
+
+namespace Common
+{
+    class cLogger
+    {
+        public static string DirectoryName = @"C:\";
+        public static string FileNameMessage = @"Service.log";
+        public static string FileNameError = @"Service.log";
+
+        private static cLogger Flog = null;
+        // возвращает указатель на самого себя это так чтобы не писать везде создание объекта этого класса
+        public static cLogger getLogger()
+        {
+            if (Flog == null)
+            {
+                Flog = new cLogger();
+
+                DirectoryName = ConfigurationManager.AppSettings["LogDirectory"].ToString();
+                FileNameMessage = ConfigurationManager.AppSettings["LogFileName"].ToString().Replace(".log", "")
+                    + DateTime.Now.Month.ToString() + "_" + DateTime.Now.Year.ToString() + ".log";
+                FileNameError = ConfigurationManager.AppSettings["LogErrorFileName"].ToString().Replace(".log", "")
+                    + DateTime.Now.Month.ToString() + "_" + DateTime.Now.Year.ToString() + ".log";
+            }
+            return Flog;
+        }
+
+        protected cLogger()
+        {
+        }
+
+        public bool logError(string Amessage)
+        {
+            if (DirectoryName.Equals(""))
+            { return false; }
+
+            StreamWriter psw;
+
+            try
+            {
+                psw = new StreamWriter(DirectoryName + FileNameError, true, Encoding.Default);
+            }
+            catch
+            {
+                return false;
+            }
+
+            try
+            {
+                psw.WriteLine(DateTime.Now.ToString() + " : " + Amessage);
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                psw.Close();
+                psw.Dispose();
+                psw = null;
+            }
+            return true;
+        }
+
+        public bool logMessage(string Amessage)
+        {
+            if (DirectoryName.Equals(""))
+            { return false; }
+
+            StreamWriter psw;
+
+            try
+            {
+                psw = new StreamWriter(DirectoryName + FileNameMessage, true, Encoding.Default);
+            }
+            catch
+            {
+                return false;
+            }
+
+            try
+            {
+                if (Amessage.Equals(""))
+                {
+                    psw.WriteLine("");
+                }
+                else
+                {
+                    psw.WriteLine(DateTime.Now.ToString() + " : " + Amessage);
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                psw.Close();
+                psw.Dispose();
+                psw = null;
+            }
+            return true;
+        }
+    }
+}
